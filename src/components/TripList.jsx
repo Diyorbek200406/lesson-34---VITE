ced05f6406ab6130d7e067cc908e3e91154a4234
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-05-18 19:53:12 +0500

    useEffect

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 const TripList = () => {
+  const [trips, setTrips] = useState([]);
+  console.log(trips);
+  useEffect(() => {
+    try {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => setTrips(data));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div>
       <h1>TripList</h1>
