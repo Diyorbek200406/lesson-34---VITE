8c1c7bd4fe1d5689e19351723edd5d0bde88b7d9
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-05-19 09:25:43 +0500

    user id ? 1

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
-import './tripList.css'
+import "./tripList.css";
 const TripList = () => {
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/users");
+  console.log("effect");
   useEffect(() => {
     try {
-      fetch("https://jsonplaceholder.typicode.com/users")
+      fetch(url)
         .then((res) => res.json())
         .then((data) => setTrips(data));
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [url]);
 
   return (
     <div className="trip-list">
       <h1>Users</h1>
       <ul>
-        {trips.map( (item) => {
+        {trips.map((item) => {
           return (
             <li key={item.id}>
               <span>{item.id} </span>
@@ -25,6 +27,24 @@ const TripList = () => {
           );
         })}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => {
+            setUrl("https://jsonplaceholder.typicode.com/users?id=1");
+            console.log("btn");
+          }}
+        >
+          European trips
+        </button>
+        <button
+          onClick={() => {
+            setUrl("https://jsonplaceholder.typicode.com/users");
+            console.log("Btn2");
+          }}
+        >
+          All trips
+        </button>
+      </div>
     </div>
   );
 };
