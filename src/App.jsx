84638e0d05a8a4593da0ae036f6333125ca5be0c
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-05-31 11:47:57 +0500

    end cUser

--- src/App.jsx
+++ src/App.jsx
@@ -6,111 +6,52 @@ import { useState } from "react";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import UserList from "./components/userList/UserList";
+import NewUserForm from "./components/newUser/newUserForm";
 
 // function arrow
 
 function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      image: "https://picsum.photos/400?random=1",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 2,
-      image: "https://picsum.photos/400?random=2",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 3,
-      image: "https://picsum.photos/400?random=3",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 4,
-      image: "https://picsum.photos/400?random=4",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 5,
-      image: "https://picsum.photos/400?random=5",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 6,
-      image: "https://picsum.photos/400?random=6",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 7,
-      image: "https://picsum.photos/400?random=7",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 8,
-      image: "https://picsum.photos/400?random=8",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-    {
-      id: 9,
-      image: "https://picsum.photos/400?random=9",
-      firstName: "Diyorbek",
-      lastName: "Erkinov",
-      age: 19,
-      from: "Uzbekistan",
-      job: "Frontend Developer",
-      gender: "Male",
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  // delete user
+  const deleteUser = (id) => {
+    setUsers((prev) => {
+      return prev.filter((user) => user.id != id);
+    });
+  };
+  // close modal
+  const closeModal = (e) => {
+    if (e.target.className === "overlay") {
+      setShowModal(false);
+    }
+    if (e.key === "Escape") {
+      setShowModal(false);
+    }
+  };
+
+  // add user
+
+  const addUser = (user) => {
+    setUsers((prev) => {
+      return [...prev, user];
+    });
+    setShowModal(false);
+  };
+
   return (
-    <div className="App">
+    <div onClick={closeModal} onKeyDown={closeModal} className="App">
       <Navbar usersLength={users.length} />
       <main>
         <div className="no-users">
           {users.length === 0 && <h2>No Users</h2>}
         </div>
-        <UserList users={users} />
+        <UserList users={users} deleteUser={deleteUser} />
       </main>
+      {showModal && <NewUserForm addUser={addUser} />}
+      <button onClick={() => setShowModal(true)} className="create-user">
+        Create User
+      </button>
       <Footer />
     </div>
   );
